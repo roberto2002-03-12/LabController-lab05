f09a27163795906ec17a1501b043116dce27f06a
roberto2002-03-12 <67761057+roberto2002-03-12@users.noreply.github.com>
2022-04-20 16:07:00 -0500

    Agregar archivos de proyecto.

---
+++ LabController/LabController/EditorCode.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LabController
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditorCode : ContentPage
+    {
+        public EditorCode()
+        {
+            InitializeComponent();
+
+            StackLayout stack = new StackLayout();
+            var MyEditor = new Editor { Text = "Im an Editor" };
+            stack.Children.Add(MyEditor);
+            Content = stack;
+        }
+    }
+}
---
+++ LabController/LabController/EmbeddedImage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Internals;
+using System.Reflection;
+
+namespace LabController
+{
+   [Preserve(AllMembers = true)]
+   [ContentProperty(nameof(Source))]
+   public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+            
+            var imageSource = ImageSource.FromResource(Source,
+                typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
